68b85c1ad413739c9faa98e3353127bfc57b2a57
Kyndall Payton <kp256@duke.edu>
2022-09-03 18:06:37 -0400

    added some imports

--- src/main/java/com/example/scenebuilderdemo/HelloApplication.java
+++ src/main/java/com/example/scenebuilderdemo/HelloApplication.java
@@ -2,16 +2,19 @@ package com.example.scenebuilderdemo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Popup;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Group group = new Group();
+        Scene scene = new Scene(group, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
